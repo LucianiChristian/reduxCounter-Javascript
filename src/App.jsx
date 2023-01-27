f9d9a42c39eac2d6d57e1138977854237560bd31
Christian Luciani <luciani_christian@yahoo.com>
2023-01-27 16:47:12 -0600

    Create basic styled Counter.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import Counter from './features/counter/Counter'
 
 export default function App() {
   return (
-    <div>App Here</div>
+    <div className="container">
+      <Counter />
+    </div>
   )
 }
---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,9 @@
+export default function Counter() {
+    return (  
+        <div className="main-counter">
+            <button type="button">+</button>
+            <p>count</p>
+            <button type="button">-</button>
+        </div>
+    );
+}
